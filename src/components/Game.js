d6f9968338bb1628e5ef320a51577fa0760517a4
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-01-14 17:22:21 +0100

    Finished the Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,45 +1,100 @@
-import React, { useReducer } from 'react';
+import React, { Component } from 'react';
 import Board from './Board';
 
-export default function Game() {
-	const [state, dispatch] = useReducer(reducer, {
-		xIsNext: true,
-		history: [{ squares: Array(9).fill(null) }],
-	});
+export default class Game extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			xIsNext: true,
+			stepNumber: 0,
+			history: [{ squares: Array(9).fill(null) }],
+		};
+	}
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
+		});
+	}
 
-	const status = 'Next player is X';
-	const moves = (
-		<li>
-			<button>Start the game!</button>
-		</li>
-	);
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		const winner = calculateWinner(squares);
+		if (winner || squares[i]) {
+			return;
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			history: history.concat({
+				squares: squares,
+			}),
+			xIsNext: !this.state.xIsNext,
+			stepNumber: history.length,
+		});
+	}
 
-	const squares = Array(9).fill(null);
+	render() {
+		const history = this.state.history;
+		const current = history[this.state.stepNumber];
+		const winner = calculateWinner(current.squares);
+		const moves = history.map((step, move) => {
+			const desc = move ? 'Go to #' + move : 'Start the Game';
+			return (
+				<li key={move}>
+					<button
+						onClick={() => {
+							this.jumpTo(move);
+						}}
+					>
+						{desc}
+					</button>
+				</li>
+			);
+		});
+		let status;
+		if (winner) {
+			status = 'Winner is ' + winner;
+		} else {
+			status = 'Next Player is ' + (this.state.xIsNext ? 'X' : 'O');
+		}
 
-	return (
-		<div className='game'>
-			<div className='game-board'>
-				<Board squares={squares}></Board>
+		return (
+			<div className='game'>
+				<div className='game-board'>
+					<Board
+						onClick={(i) => this.handleClick(i)}
+						squares={current.squares}
+					/>
+				</div>
+				<div className='game-info'>
+					<div>{status}</div>
+					<ul>{moves}</ul>
+				</div>
 			</div>
-			<div className='game-info'>
-				<div>{status}</div>
-				<ul>{moves}</ul>
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
-const reducer = (state, action) => {
-	switch (action.type) {
-		case 'MOVE':
-			return {
-				...state,
-				history: state.history.concat({
-					squares: action.payload.squares,
-				}),
-				xIsNext: !state.xIsNext,
-			};
-		default:
-			return state;
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+			return squares[a];
+		}
 	}
-};
+
+	return null;
+}
